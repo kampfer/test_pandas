e7016b2be2dd651788fb765fd258a04202a10cb2
l.w.kampfer <l.w.kampfer@gmail.com>
2017-03-03 01:01:58 +0800

    增加图表

--- example_1_2.py
+++ example_1_2.py
@@ -1,8 +1,12 @@
 #coding:utf-8
 
 import pandas as pd
+from scipy import stats
+import matplotlib.pyplot as plt
 import util
 
 data = pd.read_table('data/1/d-mmm-0111.txt', sep = '\s+', index_col=0)
 print(util.basicStats(data.rtn))
-# print(util.basicStats2(data.rtn.values))
+print(stats.ttest_1samp(data.rtn, 0))
+plt.plot(data.rtn, stats.norm.pdf(data.rtn))
+plt.show()
