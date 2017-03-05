35491b0f19b99fe581808720fea39e119c171d05
l.w.kampfer <l.w.kampfer@gmail.com>
2017-03-06 00:25:43 +0800

    修改图表

--- example_1_2.py
+++ example_1_2.py
@@ -1,12 +1,20 @@
 #coding:utf-8
 
+import numpy as np
 import pandas as pd
 from scipy import stats
 import matplotlib.pyplot as plt
 import util
 
 data = pd.read_table('data/1/d-mmm-0111.txt', sep = '\s+', index_col=0)
-print(util.basicStats(data.rtn))
+MMMStats = util.basicStats(data.rtn)
+print(MMMStats)
 print(stats.ttest_1samp(data.rtn, 0))
-plt.plot(data.rtn, stats.norm.pdf(data.rtn))
+plt.subplot(121)
+plt.plot(data)
+plt.subplot(122)
+pdf, bins, patches = plt.hist(data.rtn.values, bins=1000)
+x = np.arange(MMMStats['min'], MMMStats['max'], 0.001)
+y = stats.norm.pdf(x, MMMStats['mean'], MMMStats['std'])
+plt.plot(x, y)
 plt.show()
