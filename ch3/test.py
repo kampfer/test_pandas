966a1db91dd50be5abac19305dd10d5eabf3180b
liaowei02 <liaowei02@baidu.com>
2017-06-15 14:30:51 +0800

    add statsmodels

--- ch3/test.py
+++ ch3/test.py
@@ -3,12 +3,15 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+from statsmodels.graphics.tsaplots import plot_acf, plot_pacf
 
 data = pd.read_table('../data/ch3data/w-petroprice.txt', sep='\s+', header=0)
 data1 = pd.read_table('../data/ch3data/w-gasoline.txt', sep='\s+', squeeze=True)
 lnPrice = np.log(data.US)
 lnPrice1 = np.log(data1)
+price1Return = data1.pct_change()
 
+'''
 plt.subplot(221)
 ax = data.US.plot(title='Grude oil')
 ax.set_ylabel('price')
@@ -31,6 +34,9 @@ priceReturn1 = data1.pct_change()
 priceReturn1.plot()
 
 plt.show()
+'''
 
-plt.acorr(priceReturn1, maxlags=30)
+f = plt.figure()
+ax1 = f.add_subplot(211)
+plot_acf(price1Return, lags=31, ax=ax1)
 plt.show()
